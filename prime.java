dab4c0fff2cdaf2df215eda18827438e926bd362
xuan1006 <111722123+xuan1006@users.noreply.github.com>
2023-10-13 15:16:22 +0800

    Add files via upload
    判斷質數，不是質數印出因數

---
+++ prime.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.print("輸入數字判斷是否為prime number");
+            Scanner number = new Scanner(System.in);
+            Main.IsPrime(number.nextInt());
+        }
+    }
+    public static void IsPrime(int a){
+        ArrayList<Integer> sites = new ArrayList<Integer>();
+        if (a==2){
+            System.out.println("是質數");
+            return;
+        }
+        for(int i=2;i<a;i++){
+            if(a%i==0){
+                sites.add(i);
+            }
+        }
+
+        if(sites.isEmpty()){
+            System.out.println("是質數");
+        }else {
+            System.out.println("不是質數");
+            sites.forEach((e) -> {
+                System.out.print(e + " ");
+            });
+            System.out.println();
+        }
+    }
+}
